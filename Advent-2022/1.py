351da7b472342424e583121ad98cedbf7068cae8
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-15 06:48:28 -0500

    Day 1 complete! (list comprehension)

--- Advent-2022/1.py
+++ Advent-2022/1.py
@@ -1,1 +1,23 @@
 # Ryan Arnold - Advent 2022 - Day 1: Calorie Counting
+
+# Read in the input file and create a list of calories
+with open("input/1.txt") as f:
+    lines = f.readlines()
+carried_food = [int(line) if line != "\n" else 0 for line in lines]
+    
+# Calculate the calories for each elf.
+calories = []
+current_calories = 0
+for food in carried_food:
+    if food != 0:
+        current_calories += food
+    else:
+        calories.append(current_calories)
+        current_calories = 0
+
+# Part 1
+print(max(calories)) # 70116
+
+# Part 2
+calories.sort(reverse=True)
+print(sum(calories[:3])) # 206582
